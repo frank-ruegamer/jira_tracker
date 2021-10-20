df28bc02675db1566bfa050b5a93a5fca2ddd584
Frank Rügamer <frankruegamer@gmail.com>
2021-10-20 23:19:04 +0200

    Fix rust warnings

--- src/main.rs
+++ src/main.rs
@@ -1,18 +1,21 @@
-#[macro_use] extern crate rocket;
+#[macro_use]
+extern crate rocket;
 
 use rocket::State;
 use stopwatch::Stopwatch;
 
 mod stopwatch;
 
-static ID = Atomic
-
 #[get("/")]
 fn elapsed(stopwatch: &State<Stopwatch>) -> String {
     stopwatch.elapsed().as_secs().to_string()
 }
 
-#[launch]
-fn rocket() -> _ {
-    rocket::build().manage(Stopwatch::new_and_start()).mount("/", routes![elapsed])
+#[rocket::main]
+async fn main() {
+    let _ = rocket::build()
+        .manage(Stopwatch::new_and_start())
+        .mount("/", routes![elapsed])
+        .launch()
+        .await;
 }

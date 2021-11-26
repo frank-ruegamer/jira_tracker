1844609f93e17cac433596a0345acc36e4f7f554
Frank Rügamer <frankruegamer@gmail.com>
2021-11-27 00:45:02 +0100

    Use Mutex to modify stopwatch in routes

--- src/main.rs
+++ src/main.rs
@@ -2,21 +2,42 @@
 extern crate rocket;
 
 use rocket::State;
+use std::sync::Mutex;
 use stopwatch::Stopwatch;
 
 mod instant_serializer;
 mod stopwatch;
 
+struct AppData {
+    stopwatch: Mutex<Stopwatch>,
+}
+
 #[get("/")]
-fn elapsed(stopwatch: &State<Stopwatch>) -> String {
-    humantime::format_duration(stopwatch.elapsed_seconds()).to_string()
+fn elapsed(app_data: &State<AppData>) -> String {
+    let stopwatch = app_data.stopwatch.lock().unwrap();
+    humantime::format_duration(stopwatch.total_elapsed_seconds()).to_string()
+}
+
+#[post("/start")]
+fn start(app_data: &State<AppData>) {
+    let mut stopwatch = app_data.stopwatch.lock().unwrap();
+    stopwatch.start();
+}
+
+#[post("/pause")]
+fn pause(app_data: &State<AppData>) {
+    let mut stopwatch = app_data.stopwatch.lock().unwrap();
+    stopwatch.pause();
 }
 
 #[rocket::main]
 async fn main() {
+    let state = AppData {
+        stopwatch: Mutex::new(Stopwatch::new_and_start()),
+    };
     let _ = rocket::build()
-        .manage(Stopwatch::new_and_start())
-        .mount("/", routes![elapsed])
+        .manage(state)
+        .mount("/", routes![elapsed, start, pause])
         .launch()
         .await;
 }

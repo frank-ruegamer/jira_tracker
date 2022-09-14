fc4736dc121e816217d1e5284f425a520a2aeb9e
Frank Rügamer <frankruegamer@gmail.com>
2022-09-14 16:13:28 +0200

    Inline bash command for awesome widget

--- external/awesome-wm-widget/tempo.lua
+++ external/awesome-wm-widget/tempo.lua
@@ -7,6 +7,17 @@ local function update_tempo_status(_, stdout)
   tempo_widget:set_text(stdout)
 end
 
-watch("bash -c '$HOME/.config/awesome/widgets/tempo-tracker/tracker.bash'", 5, update_tempo_status)
+local command = [[
+  set -eo pipefail
+
+  tracker=$(bash -c 'curl -s localhost:8000/tracker')
+  key=$(jq -r .key <<< "${tracker}")
+  description=$(jq -r --arg key "${key}" '.[] | select(.key == $key) | .fields.summary' ~/.jira/issues.json)
+  jq -r --arg description "${description}" '"[" + .key + "] " + $description + ": " + .duration' <<< "${tracker}"
+]]
+
+local watch_command = string.format([[ bash -c "%s" ]], command:gsub('"', '\\"'))
+
+watch(watch_command, 5, update_tempo_status)
 
 return tempo_widget
--- external/awesome-wm-widget/tracker.bash
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-set -eo pipefail
-
-tracker=$(bash -c 'curl -s localhost:8000/tracker')
-key=$(jq -r .key <<< "${tracker}")
-description=$(jq -r --arg key "${key}" '.[] | select(.key == $key) | .fields.summary' ~/.jira/issues.json)
-jq -r --arg description "${description}" '"[" + .key + "] " + $description + ": " + .duration' <<< "${tracker}"

aac537d9b80abbe33df204aec630e4e5e71b9118
Frank Rügamer <frankruegamer@gmail.com>
2025-09-10 16:41:46 +0200

    Slightly update rofi_jira_tracker

--- rofi_jira_tracker/src/lib.rs
+++ rofi_jira_tracker/src/lib.rs
@@ -1,33 +1,71 @@
+extern crate core;
+
 use domain::TrackerInformation;
 use rofi_mode::cairo::Surface;
 use rofi_mode::{export_mode, Action, Api, Event, Matcher};
+use std::future::Future;
+use std::time::Duration;
 
 export_mode!(Mode);
 
 struct Mode<'rofi> {
     api: Api<'rofi>,
-    trackers: Vec<TrackerInformation>,
+    entries: Vec<Entry>,
+}
+
+enum Entry {
+    Tracker(TrackerInformation),
+    NewTracker,
+    Refresh,
+    Submit,
+}
+
+impl From<&Entry> for rofi_mode::String {
+    fn from(value: &Entry) -> Self {
+        match value {
+            Entry::Tracker(tracker) => (&tracker.key).into(),
+            Entry::NewTracker => "Add New".into(),
+            Entry::Refresh => "Refresh issues".into(),
+            Entry::Submit => "Submit".into(),
+        }
+    }
+}
+
+async fn get() -> String {
+    tokio::time::sleep(Duration::from_secs(1)).await;
+    "1234".into()
 }
 
 impl<'rofi> rofi_mode::Mode<'rofi> for Mode<'rofi> {
     const NAME: &'static str = "jira-tracker\0";
 
     fn init(api: Api<'rofi>) -> Result<Self, ()> {
-        let response = reqwest::blocking::get("http://localhost:8000/trackers").map_err(|_| ())?;
-        let trackers = response.json::<Vec<TrackerInformation>>().map_err(|_| ())?;
-        Ok(Mode { api, trackers })
+        get()
+        let response = reqwest::blocking::get("http://localhost:8081/trackers").map_err(|e| {
+            eprintln!("{}", e);
+            ()
+        })?;
+        let trackers = response.json::<Vec<TrackerInformation>>().map_err(|e| {
+            eprintln!("{}", e);
+            ()
+        })?;
+        let mut entries: Vec<_> = trackers
+            .into_iter()
+            .map(|tracker| Entry::Tracker(tracker))
+            .collect();
+
+        entries.push(Entry::NewTracker);
+        entries.push(Entry::Refresh);
+        entries.push(Entry::Submit);
+        Ok(Mode { api, entries })
     }
 
     fn entries(&mut self) -> usize {
-        self.trackers.len()
+        self.entries.len()
     }
 
     fn entry_content(&self, line: usize) -> rofi_mode::String {
-        if line < self.trackers.len() {
-            self.trackers[line].key.clone().into()
-        } else {
-            unreachable!()
-        }
+        (&self.entries[line]).into()
     }
 
     fn entry_icon(&mut self, _line: usize, _height: u32) -> Option<Surface> {
@@ -35,7 +73,21 @@ impl<'rofi> rofi_mode::Mode<'rofi> for Mode<'rofi> {
     }
 
     fn react(&mut self, _event: Event, _input: &mut rofi_mode::String) -> Action {
-        Action::Exit
+        match _event {
+            Event::Ok { alt, selected } => {
+                println!("alt {}, selected {}", alt, selected);
+                match &self.entries[selected] {
+                    Entry::Tracker(tracker) => {
+                        println!("key {:?}", tracker);
+                    }
+                    Entry::NewTracker => {}
+                    Entry::Refresh => {}
+                    Entry::Submit => {}
+                }
+                Action::Exit
+            }
+            _ => Action::Exit,
+        }
     }
 
     fn matches(&self, line: usize, matcher: Matcher<'_>) -> bool {
